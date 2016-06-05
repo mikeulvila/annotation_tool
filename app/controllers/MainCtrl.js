20e1dcf7ae38a984d179b5da268c8164ac065024
Mike Ulvila <mikeulvila@me.com>
2016-06-05 14:40:18 -0500

    replace line breaks with <br> tag

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -3,15 +3,17 @@ angular.module('DR_Annotation')
   .controller('MainController', ['$scope', '$state', 'DataService',
     function($scope, $state, DataService) {
 
-      $scope.chapter = "Chapter 8"
+      $scope.chapterTitle = "Chapter 8"
+      $scope.chapterText = "";
 
       DataService.getChapter()
-        .then(function(response) {
-          console.log("RESPONSE", response);
+        .then(function(chapter) {
+          console.log("Chapter", chapter);
+          $scope.chapterText = chapter.data.replace(/\n/g,"<br>");
         })
         .catch(function (error) {
            console.log("ERROR", error);
         })
-      $scope.test = "This is a test of $scope";
+
 
 }]);

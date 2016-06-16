d5e81a0015c62852f2ec809d5c17f4484da9400d
Mike Ulvila <mikeulvila@me.com>
2016-06-16 13:16:52 -0500

    save to JSON feature added, annotation mode added

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -1,27 +1,39 @@
 // app/controllers/MainCtrl.js
 angular.module('DR_Annotation')
-  .controller('MainController', ['$scope', '$state', 'DataService',
-    function($scope, $state, DataService) {
+  .controller('MainController', ['$scope', '$state', '$log', 'DataService',
+    function($scope, $state, $log, DataService) {
 
       $scope.error;
+      $scope.annotateMode = false;
+      $scope.chapterText = "";
       $scope.annotatedChapterText = "";
-      var originalChapterText = "";
-      $scope.annotationArray = [];
+      $scope.annotationArray;
       $scope.selectChapter;
 
       $scope.loadChapter = function () {
         loadChapterAndAnnotations($scope.selectChapter);
-      }
+      };
+
+      $scope.save = function () {
+        var annotationsJSON = angular.toJson($scope.annotationArray, true);
+        if (annotationsJSON) {
+          $log.info(annotationsJSON);
+        } else {
+          $log.warn('There are no annotations for the current selected chapter.')
+        }
+      };
 
       function loadChapterAndAnnotations (chapterNumber) {
 
         DataService.getChapterText(chapterNumber)
           .then(function(chapter) {
-            $scope.annotatedChapterText = chapter.data;  //.replace(/\n/g,"<br>");
+
+            $scope.chapterText = chapter.data;
 
             DataService.getChapterAnnotations(chapterNumber)
               .then(function (response) {
 
+                $scope.annotatedChapterText = chapter.data;  //.replace(/\n/g,"<br>");
                 $scope.annotationArray = response.data.document.span.reverse();
 
                 angular.forEach($scope.annotationArray, function (value, index) {
@@ -29,12 +41,12 @@ angular.module('DR_Annotation')
                    var end = parseInt(value.extent.charseq._END) + 1;
                    var subText = $scope.annotatedChapterText.substring(start, end);
                    var category = value._category;
+                   var arrayIndex = index.toString();
                    var preText = $scope.annotatedChapterText.substring(0, start);
                    var hiliteText = '<span class="' + category + '">' + subText + '</span>';
                    var postText = $scope.annotatedChapterText.substring(end);
 
                    $scope.annotatedChapterText = preText + hiliteText + postText;
-
                 });
 
                 $scope.annotationArray.reverse();
@@ -42,21 +54,24 @@ angular.module('DR_Annotation')
               })
               .catch(function (error) {
                 $scope.error = error;
+                $log.error(error.status, error.statusText);
               });
+
           })
           .catch(function (error) {
              $scope.error = error;
-             console.log("ERROR", error);
+             $log.error(error.status, error.statusText);
           });
-      } // end loadChapterAndAnnotations function
+
+
+      }; // end loadChapterAndAnnotations function
 
       $scope.selectText = function (event) {
         var selection = document.getSelection();
         var start = selection.anchorOffset < selection.focusOffset ? selection.anchorOffset : selection.focusOffset;
         var end = selection.anchorOffset < selection.focusOffset ? selection.focusOffset - 1: selection.anchorOffset - 1;
         console.log('Selection', selection, start, end); // also think about getRangeAt(0) or createRange()
-      }
-
+      };
 
 
 }]);

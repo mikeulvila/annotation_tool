416eeb847f507f2cd2555e6b1c447236d01578ef
Mike Ulvila <mikeulvila@me.com>
2016-06-17 00:34:40 -0500

    createAnnotatedChapterText function

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -47,39 +47,37 @@ angular.module('DR_Annotation')
 
             DataService.getChapterAnnotations(chapterNumber)
               .then(function (response) {
-
                 $scope.annotatedChapterText = chapter.data;  //.replace(/\n/g,"<br>");
                 $scope.annotationArray = response.data.document.span.reverse();
-
-                angular.forEach($scope.annotationArray, function (value, index) {
-                   var start = parseInt(value.extent.charseq._START) ;
-                   var end = parseInt(value.extent.charseq._END) + 1;
-                   var subText = $scope.annotatedChapterText.substring(start, end);
-                   var category = value._category;
-                   var arrayIndex = index.toString();
-                   var preText = $scope.annotatedChapterText.substring(0, start);
-                   var hiliteText = '<span data-index="' + arrayIndex + '" class="' + category + '">' + subText + '</span>';
-                   var postText = $scope.annotatedChapterText.substring(end);
-
-                   $scope.annotatedChapterText = preText + hiliteText + postText;
-                });
-
-                $scope.annotatedChapterText = $sce.trustAsHtml($scope.annotatedChapterText);
-
+                $scope.annotatedChapterText =
+                $sce.trustAsHtml(createAnnotatedChapterText($scope.annotatedChapterText, $scope.annotationArray));
               })
               .catch(function (error) {
                 $scope.error = error;
                 $log.error(error.status, error.statusText);
               });
-
           })
           .catch(function (error) {
              $scope.error = error;
              $log.error(error.status, error.statusText);
           });
-
-
       }; // end loadChapterAndAnnotations function
+
+      function createAnnotatedChapterText (text, annotations) {
+        angular.forEach(annotations, function (value, index) {
+          var start = parseInt(value.extent.charseq._START) ;
+          var end = parseInt(value.extent.charseq._END) + 1;
+          var subText = text.substring(start, end);
+          var category = value._category;
+          var arrayIndex = index.toString();
+          var preText = text.substring(0, start);
+          var hiliteText =
+          '<span data-index="' + arrayIndex + '" class="' + category + '">' + subText + '</span>';
+          var postText = text.substring(end);
+          text = preText + hiliteText + postText;
+        });
+        return text;
+      };
 
       $scope.selectText = function (event) {
         var selection = document.getSelection();
